f5dd6bf19ef3c9b16b1667ab8bde5d0708c067b2
Yi Yang <weiy@bxscience.edu>
2019-07-19 10:28:06 -0400

    hi

--- data_code.py
+++ data_code.py
@@ -1,126 +1,123 @@
-from wordcloud import WordCloud
-
-wordcloud = WordCloud().generate(tweet_texts)
-
-plt.imshow(wordcloud, interpolation = "bilinear")
-plt.axis("off")
-plt.show()
-plt.savefig('chart.png')
-#tweet_texts[]
-tweetstring = ""
-for tweet in tweet_texts:
-	tweet = tweet + " "
-	tweet_texts == tweet_texts
-print(tweetstring)
 
 #----------------------------------
 import json
 from textblob import TextBlob
+from wordcloud import WordCloud
 import matplotlib.pyplot as plt
 
+# def numOfLetter(tw, letter):
+# 	count = 0
+# 	for l in tw:
+# 		if l == letter or l.lower() == letter:
+# 			#print(l)
+# 			count += 1
+# 	return count
+
 tweetFile = open("tweets_small.json", "r")
 tweetData = json.load(tweetFile)
-#print(tweetData[0].keys())
+# print(tweetData[0].keys())
 tweetFile.close()
-
-# tb = TextBlob("You are a brilliant computer scientist.")
-# print(tb.polarity)
-
-polarityList =[]
-# for item in listOfTweets:
-# 	blob = TextBlob(item)
-# 	polar1 = blob1.polarity
-# 	polarityList.append(polar1)
-#------------------------------------
-# texts = [tweet_texts]
-# polarity_list = [0.0, 0.9...]
-# list = []
-# #tweetsData[i]["id"]
-# for i in range (len(tweetsData)):
-# 	dictionary = {}
-# 	dictionary["t_id"] = tweetsData[i]["id"]
-# 	dictionary["polarity"] = polarity_list[i]
-# 	dictionary["tweet"] = texts[i]
-# 	list.append(dictionary)
-	#------------------------
-
-
-# '''
-# In this program, we print out all the text data from our twitter JSON file.
-# Please explain the comments to students as you code.
-# '''
-
-# We start by importing the JSON library to use for this project.
-# Twitter data is stored in this format - this is the same format
-# students learned for their Survey Project!
-# import json
-#
-# # Next we want to open the JSON file. We tag this file as
-# # "r" read only because we are only going to look at the data.
-tweetFile = open("tweets_small.json", "r")
-tweetData = json.load(tweetFile)
 
 # print(type(tweetData))
 # print(type(tweetData[0]))
 # print(tweetData[0]["favorite_count"])
 
-favoriteCount, numberofFavoriteCounts = 0, 0
-for i in range(0, len(tweetData)):
-	if "favorite_count" in tweetData[i]:
-		favoriteCount += tweetData[i]["favorite_count"]
-		numberofFavoriteCounts += 1
-avg = favoriteCount / numberofFavoriteCounts
-# print(avg)
-#
-# # tweetData[0]["text"]
-# # for tweet in tweetData:
-# # 	if "text" in tweet:
-# # 		text = tweet["text"]
-# # 		for index in range(26):
-# # 			print(index)
-#
+# favoriteCount, numberofFavoriteCounts = 0, 0
+# for i in range(0, len(tweetData)):
+# 	if "favorite_count" in tweetData[i]:
+# 		favoriteCount += tweetData[i]["favorite_count"]
+# 		numberofFavoriteCounts += 1
+# avg = favoriteCount / numberofFavoriteCounts
+# # print(avg)
+# #
+# # # tweetData[0]["text"]
+# # # for tweet in tweetData:
+# # # 	if "text" in tweet:
+# # # 		text = tweet["text"]
+# # # 		for index in range(26):
+# # # 			print(index)
+# #
 tweet_texts = []
-for t in range(0, len(tweetData)):
-	if "text" in tweetData[t]:
+for t in range(len(tweetData)):
 		tweet_texts.append(tweetData[t]["text"])
-# print(tweet_texts)
-#
-for i in range(0,len(tweet_texts)):
-	blob1 = TextBlob(tweet_texts[i])
+print(tweet_texts)
+
+# tweetID = []
+# for i in range(len(tweetData)):
+# 	tweetList.append(tweetData [i]["id"])
+# print(type(tweetID))
+'''
+polarityList =[]
+for item in tweet_texts:
+	blob1 = TextBlob(item)
 	polar1 = blob1.polarity
 	polarityList.append(polar1)
+#print(polarityList)
+
+tweetpol = {}
+dictlist = []
+for i in range(0,len(tweetData)):
+	poldict = {}
+	poldict["twitterid"] = tweetData[i]["id"]
+	poldict["polarity"] = polarityList[i]
+	poldict["tweet"] = tweet_texts[i]
+	dictlist.append(poldict)
+# print(dictlist)
+'''
+
+def countLetter(string, letter):
+	counter = 0
+	for let in string:
+		if let.lower() == letter:
+			counter += 1
+		else:
+			counter += 0
+		# print(counter)
+	return counter
+
+tweetstring = ""
+for tweet in tweet_texts:
+	tweet = tweet + " "
+	tweetstring += tweet
+print(tweetstring)
+
+countLetter(tweetstring, "a")
+letters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+print(letters)
+
+occurrences = []
+for letter in letters:
+	occurrences.append(countLetter(tweetstring, letter))
+	# print(f"letter:{letter} occurrences:{countLetter(tweetstring, letter)}")
+print(occurrences)
+print(min(occurrences), max(occurrences))
+plt.hist(occurrences)
+plt.axis([min(occurrences), max(occurrences), 0, 10])
+plt.show()
+# plt.savefig("chart.png")
+
+
+# wordcloud = WordCloud(height = 1000, width = 1000).generate(tweetstring)
+# plt.figure(figsize = (10,10), facecolor = None)
+# plt.imshow(wordcloud, interpolation = "bilinear")
+# plt.axis("off")
+# plt.show()
+# plt.savefig('chart.png')
+# #tweet_texts[]
+#
 # print(polarityList)
-#
-#
-# We can close the file now that we have locally stored the data.
-tweetFile.close()
+# print(min(polarityList), max(polarityList))
+# plt.hist(polarityList)
+# plt.axis([min(polarityList), max(polarityList), 0, 50])
+#plt.show()
+# plt.savefig("chart.png")
 
-# We then print the data of ONE tweet:
-# the [0] denotes the *first* tweet object.
-# # We access parts of the tweet using ["NameOfPart"].
+# tweetFile.close()
 # print("Tweet id: ", tweetData[0]["id"])
-#
-# # First ask students how they might print the text object:
-# # Then show them the following code
 # print("Tweet text: ", tweetData[0]["text"])
-
-
-# First ask students how might they use loops
-# to print the ["text"] of all the tweets:
-
-# Show them the following two options:
-
-# Explain how here, we're using index and
-# counting the number of tweets in the tweetData
-# using the python len() function.
+#
 # for idx in range(len(tweetData)):
 # 	print("Tweet text: " + tweetData[idx]["text"])
 #
-# # Explain here how Python lets you get objects
-# # directly without having to use an index.
 # for tweet in tweetData:
 # 	print("Tweet text: " + tweet["text"])
-
-# Encourage students to think about how there are
-# often multiple solutions for each problem, and
-# how each solution might have strenghts and drawbacks.
