38d1ba23a2194f97fde30b501a1b846d36fa8b95
Yi Yang <weiy@bxscience.edu>
2019-07-23 13:33:17 -0400

    s

--- helloworld.js
+++ helloworld.js
@@ -1,3 +1,7 @@
 console.log("Hello World");
 
 var h1tag = document.getElementsByTagName('h1')[0];
+
+var loc = document.getElementsByClassName("headings")[3];
+
+var adi = ["brave", "resilient", "lovely", "happy"]
