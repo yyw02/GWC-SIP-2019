a2327ed41452c0f73a80366c965f750cef0ff3d2
Yi Yang <weiy@bxscience.edu>
2019-07-23 13:10:32 -0400

    s

--- index.js
+++ index.js
@@ -5,10 +5,30 @@ for (i = 0; i < 21; i+=2){
   console.log(i);
 }
 
-i = 0;
-while( i < 21){
+i = 0; // sets i to zero
+while( i <= 20){  // runs when i <= 20
   if(i%2 == 0){
     console.log(i);
   }
   i += 1;
 }
+
+
+function getTemp(){
+  return 22.5;
+}
+var temperature = getTemp();
+console.log(temperature);
+
+function getTemp2(type){
+  if (type == "C"){
+    alert("It is rainy today!");
+    return 22.5;
+  }
+  if (type == "F"){
+    alert("It is really hot today!");
+    return 100;
+  }
+}
+console.log(getTemp2("C"))
+console.log(getTemp2("F"))
