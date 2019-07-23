3ef091873b4bd91547e215fc3942144089ccf0eb
Yi Yang <weiy@bxscience.edu>
2019-07-23 10:55:33 -0400

    s

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("Hello World!")
+console.log("Hello World!");
+
+var i = 0;
+for (i = 0; i < 21; i+=2){
+  console.log(i);
+}
+
+i = 0;
+while( i < 21){
+  if(i%2 == 0){
+    console.log(i);
+  }
+  i += 1;
+}
