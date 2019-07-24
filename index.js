3049c36fee5fc631b4c807fc9f1b8141e13ce04b
Yi Yang <weiy@bxscience.edu>
2019-07-24 15:29:58 -0400

    123456789

--- index.js
+++ index.js
@@ -1,43 +1,44 @@
-console.log("Hello World!");
-
-var i = 0;
-for (i = 0; i < 21; i+=2){
-  console.log(i);
-}
-
-i = 0; // sets i to zero
-while( i <= 20){  // runs when i <= 20
-  if(i%2 == 0){
-    console.log(i);
-  }
-  i += 1;
-}
-
-
-function getTemp(){
-  return 22.5;
-}
-var temperature = getTemp();
-console.log(temperature);
-
-function getTemp2(type){
-  if (type == "C"){
-    alert("It is rainy today!");
-    return 22.5;
-  }
-  if (type == "F"){
-    alert("It is really hot today!");
-    return 100;
-  }
-}
-console.log(getTemp2("C"))
-console.log(getTemp2("F"))
-
-
+// console.log("Hello World!");
+//
+// var i = 0;
+// for (i = 0; i < 21; i+=2){
+//   console.log(i);
+// }
+//
+// i = 0; // sets i to zero
+// while( i <= 20){  // runs when i <= 20
+//   if(i%2 == 0){
+//     console.log(i);
+//   }
+//   i += 1;
+// }
+//
+//
+// function getTemp(){
+//   return 22.5;
+// }
+// var temperature = getTemp();
+// console.log(temperature);
+//
+// function getTemp2(type){
+//   if (type == "C"){
+//     alert("It is rainy today!");
+//     return 22.5;
+//   }
+//   if (type == "F"){
+//     alert("It is really hot today!");
+//     return 100;
+//   }
+// }
+// console.log(getTemp2("C"))
+// console.log(getTemp2("F"))
 
 
-document.getElementsById("specialfont").addEventListener("click", function name(){
-    alert("Hello WORLD")
-    document.getElementById("specialfont").style.color = "red";
+
+
+document.getElementsById("specialfont").addEventListener("click",
+    function name(){
+    alert("Hello WORLD");
+    document.getElementById("specialfont").style.color = "blue";
   }
 )
