d65568d81714761f44f3f4fec4569f7e9f630345
Yi Yang <weiy@bxscience.edu>
2019-07-24 10:38:55 -0400

    z

--- helloworld.js
+++ helloworld.js
@@ -1,15 +1,29 @@
 var adj = ["brave", "resilient", "lovely", "happy", "lvely","friendly", "kind"];
 var pos = 0;
-var loc = document.getElementById("abc");
+var loc = document.getElementById("abcdefghij");
 
-function changeAdj(){
-  loc.innerHTML = adjectives[pos];
-  pos ++;
-  if (pos >= adjectives.length){
-    pos =0;
-    }
-  }
+// function changeAdj(){
+//   loc.innerHTML = adjectives[pos];
+//   pos ++;
+//   if (pos >= adjectives.length){
+//     pos = 0;
+//     }
+//   }
 
-var x = document.getElementsByTagName("body")[0]
+var x = document.getElementById("colorChange")
 function colorfulBackground(){
-  x.setAttribute("style", `background-color:rgb(${Math.floor(Math.random() * 256)},${Math.floor(Math.random() * 256)},${Math.floor(Math.random() * 256)})`)}
+  x.setAttribute("style", `background-color:rgb(${Math.floor(Math.random() * 256)},${Math.floor(Math.random() * 256)},${Math.floor(Math.random() * 256)})`)
+}
+
+  var foz = document.getElementById("the name")
+ function changeFontColor(){
+   foz.setAttribute("style", "color:blue");
+ }
+
+
+
+var font = ["'Bonbon', cursive;","'Butcherman', cursive;", "'Fascinate', cursive;", "'Indie Flower', cursive;", "'Lobster', cursive;", "'Abel', sans-serif;"];
+var poz = 0;
+function fontChange(){
+  foz.setAttribute("style",)
+}
